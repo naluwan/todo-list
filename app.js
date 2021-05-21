be6e580ace32ae5838b9c9c9a4b97275ef53c621
naluwan <street.baller06@gmail.com>
2021-05-21 17:06:47 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 const port = 3000
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: trueç })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('This will be a todo-list website by Express')
