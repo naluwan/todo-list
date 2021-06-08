fa69e2302ebbc3bf01ab1a4a8e84849616d84e3c
naluwan <street.baller06@gmail.com>
2021-06-08 11:34:55 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require('express')
 const hbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const port = 3000
 
 const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
+const port = process.env.port || 3000
 
 // setting template engine
 app.engine('hbs', hbs({ defaultLayout: 'main', extname: '.hbs' }))
